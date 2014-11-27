c92cd3759e0b8da557f915b4d12c91139550ec74
deadmask <luca.anghileri@gmail.com>
2014-11-28 00:23:44 +0100

    1

---
+++ Chapter 5/Razor/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Razor.Models;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        Product product = new Product
+        {
+            ProductID = 1,
+            Name = "Kayak",
+            Description = "A bot for one person",
+            Category = "Watersports",
+            Price = 275M
+        };
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(product);
+        }
+
+        public ActionResult NameAndPrice()
+        {
+            return View(product);
+        }
+
+    }
+}
